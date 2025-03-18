5b1315f47e514ad16305cf113c54cb1c3a35df4f
Mikhail K <mikhail.kamochkin@ontariotechu.net>
2025-03-18 17:15:49 -0400

    added student view and controller

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,123 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DotNetCoreSqlDb.Data;
+using DotNetCoreSqlDb.Models;
+
+namespace DotNetCoreSqlDb.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly MyDatabaseContext _context;
+
+        public StudentsController(MyDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Students
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Student.ToListAsync());
+        }
+
+        // GET: Students/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var student = await _context.Student.FirstOrDefaultAsync(m => m.ID == id);
+            if (student == null)
+                return NotFound();
+
+            return View(student);
+        }
+
+        // GET: Students/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Students/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Name,ParentOrEmployer,MainNotes,OngoingNotes,Email,PhoneNumber,CreatedDate")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(student);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
+
+        // GET: Students/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var student = await _context.Student.FindAsync(id);
+            if (student == null)
+                return NotFound();
+            
+            return View(student);
+        }
+
+        // POST: Students/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,ParentOrEmployer,MainNotes,OngoingNotes,Email,PhoneNumber,CreatedDate")] Student student)
+        {
+            if (id != student.ID)
+                return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(student);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.Student.Any(e => e.ID == student.ID))
+                        return NotFound();
+                    else
+                        throw;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
+
+        // GET: Students/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var student = await _context.Student.FirstOrDefaultAsync(m => m.ID == id);
+            if (student == null)
+                return NotFound();
+
+            return View(student);
+        }
+
+        // POST: Students/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var student = await _context.Student.FindAsync(id);
+            if (student != null)
+            {
+                _context.Student.Remove(student);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
