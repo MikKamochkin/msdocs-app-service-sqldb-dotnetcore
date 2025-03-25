24f13fa25602e3f999cee7af859cc0ea3a5cac0f
Mikhail K <mikhail.kamochkin@ontariotechu.net>
2025-03-25 14:23:07 -0400

    default opening window is login

--- Program.cs
+++ Program.cs
@@ -45,6 +45,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Login}/{action=Login}/{id?}");
+    pattern: "{controller=Login}/{action=Index}/{id?}");
 
 app.Run();
